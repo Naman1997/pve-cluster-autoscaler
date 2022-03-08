9d3022759d7407523ecf933bf1bd5112804dd194
Naman Arora <aroranaman17@outlook.com>
2022-03-08 17:15:09 +0530

    Added postgres

--- src/proxmox-api.go
+++ src/proxmox-api.go
@@ -8,11 +8,11 @@ import (
 )
 
 /*
-Clone function creates a new clone
+Creates a new clone
 of the provided template and
 configures it according to cloudInitConfig
 */
-func Clone(client *proxmox.Client, template string, cloudInitConfig []byte, node string) {
+func Clone(client *proxmox.Client, template string, cloudInitConfig []byte, node string) *proxmox.VmRef {
 	config, err := proxmox.NewConfigQemuFromJson(bytes.NewReader(cloudInitConfig))
 	FailError(err)
 	log.Println("Looking for template: " + template)
@@ -20,7 +20,6 @@ func Clone(client *proxmox.Client, template string, cloudInitConfig []byte, node
 	FailError(err)
 	if sourceVmrs == nil {
 		log.Fatal("Can't find template")
-		return
 	}
 	vmid, err := client.GetNextID(0)
 	FailError(err)
@@ -38,12 +37,15 @@ func Clone(client *proxmox.Client, template string, cloudInitConfig []byte, node
 
 	FailError(config.CloneVm(sourceVmr, vmr, client))
 	FailError(config.UpdateConfig(vmr, client))
-	log.Println("Complete")
+	err = proxmox.WaitForShutdown(vmr, client)
+	FailError(err)
+	log.Println("Completed cloning process")
+	vmr = proxmox.NewVmRef(config.VmID)
+	return vmr
 }
 
 /*
-Destroy function stops and
-deletes am existing VM
+Deletes an existing VM
 using its vmid
 */
 func Destroy(client *proxmox.Client, vmid int) {
@@ -54,4 +56,26 @@ func Destroy(client *proxmox.Client, vmid int) {
 	jbody, err = client.DeleteVm(vmr)
 	FailError(err)
 	ColorPrint(INFO, jbody)
+}
+
+/*
+Starts an existing VM
+using its vmid
+*/
+func Start(client *proxmox.Client, vmid int) string {
+	vmr := proxmox.NewVmRef(vmid)
+	jbody, err := client.StartVm(vmr)
+	FailError(err)
+	return jbody
+}
+
+/*
+Stops an existing VM
+using its vmid
+*/
+func Stop(client *proxmox.Client, vmid int) string {
+	vmr := proxmox.NewVmRef(vmid)
+	jbody, err := client.StopVm(vmr)
+	FailError(err)
+	return jbody
 }
